c994761ffe82c8fa284fc35377ca24f2d25d6a6a
Ashwin <ashwinammanagi@gmail.com>
2023-07-17 18:24:39 -0400

    css added till before video rows

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,37 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <h1>{{ title }}</h1>
+    <Divider />
     <form @submit.prevent="calculateLength">
-      <input type="text" placeholder="youtube.com/playlist?list=ID" v-model="youtubePlaylistId">
-      <button type="button" @click="calculateLength" :disabled="isLinkInvalid">SUBMIT</button>
-      <p>{{ invalidLink }}</p>
+      <InputText type="text" placeholder="Paste YouTube Playlist Link" v-model="youtubePlaylistId" :class="{'p-invalid': invalidLink !== '' && youtubePlaylistId !== ''}" />
+      <Button label="Submit" @click="calculateLength" v-if="isLinkInvalid != true" />
+      <InlineMessage severity="error" v-if="invalidLink != '' && youtubePlaylistId != ''">{{ invalidLink }}</InlineMessage>
     </form>
 
     <div>
       <span>Playback Speed: {{ playbackSpeed }}x</span>
       <br>
-      <input type="range" v-model="playbackSpeed" min="0.25" max="2" step="0.25">
+      <Slider type="range" v-model="playbackSpeed" :min="0.25" :max="2" :step="0.25" class="w-14rem" />
     </div>
-
+    
     <div v-if="response && response.data && response.data.video_list !== 'Invalid link'">
       <h2>Playlist duration: {{ formatDuration(adjustedDuration) }}</h2>
-      <ul>
+      <Divider />
+      <ul style="list-style: none;">
         <li v-for="video in response.data.video_list" :key="video.snippet.title">
-          <a :href="'https://www.youtube.com/watch?v=' + video.snippet.resourceId.videoId">
-            <img :src="video.snippet.thumbnails.high.url" alt="Thumbnail">
-            <p>Title: {{ video.snippet.title }}</p>
-          </a>
+          <a :href="'https://www.youtube.com/watch?v=' + video.snippet.resourceId.videoId" style="text-decoration:none">
+            <Card style="width: 20em">
+              <template #header>
+                  <img alt="Thumbnail" :src="video.snippet.thumbnails.medium.url" style="width: 20em" />
+              </template>
+              <template #subtitle> {{ video.snippet.position + 1 }}. {{ video.snippet.title }} </template>
+            </Card>
+          </a><br>
         </li>
       </ul>
     </div>
-
-
+ 
+    <ScrollTop />
   </div>
 </template>
 
@@ -110,9 +116,9 @@ export default {
 }
 </script>
 
-<style>
+<!-- <style>
 #app {
   text-align: center;
   font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 }
-</style>
+</style> -->
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+import InlineMessage from 'primevue/inlinemessage';
+import ScrollTop from 'primevue/scrolltop';
+import Slider from 'primevue/slider';
+import Card from 'primevue/card';
+import Divider from 'primevue/divider';
 
-createApp(App).mount('#app')
+// theme
+import 'primevue/resources/themes/vela-blue/theme.css';
+import 'primevue/resources/themes/vela-blue/theme.css';
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.component('InputText', InputText);
+app.component('Button', Button);
+app.component('InlineMessage', InlineMessage);
+app.component('ScrollTop', ScrollTop);
+app.component('Slider', Slider);
+app.component('Card', Card);
+app.component('Divider', Divider);
+app.mount('#app')
